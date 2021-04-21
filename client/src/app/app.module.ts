c0b0209f05b917613a7e8f0596bf6715cc563997
Anna Panas <annapanas0906@gmail.com>
2021-04-20 22:51:01 -0400

    styled sign up component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FriendProfileModule } from '../components/friend-profile/friend-profile
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { AngularFireStorageModule } from '@angular/fire/storage';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
